cf729039e9cde653d2e8a6e38d6b4f92a884e8c0
bigmagic <fujin.zhao@foxmail.com>
2021-03-25 13:40:24 +0800

    led blink

--- projects/2_led_demo/application/main.c
+++ projects/2_led_demo/application/main.c
@@ -5,16 +5,52 @@
 #include "nuclei_sdk_soc.h"
 #include "nuclei_sdk_hal.h"
 
-void led_config(void)
+#define LED_G   LED1
+#define LED_B   LED2
+#define LED_R   LED3
+
+#define LED_G_ON     gd_rvstar_led_on(LED_G)
+#define LED_G_OFF    gd_rvstar_led_off(LED_G)
+
+#define LED_B_ON     gd_rvstar_led_on(LED_B)
+#define LED_B_OFF    gd_rvstar_led_off(LED_B)
+
+#define LED_R_ON     gd_rvstar_led_on(LED_R)
+#define LED_R_OFF    gd_rvstar_led_off(LED_R)
+
+//gpio init
+void user_gpio_init(void)
 {
-    for(int i=0; i<LEDn; i++)
+    gd_rvstar_led_init(LED_G);
+    gd_rvstar_led_init(LED_B);
+    gd_rvstar_led_init(LED_R);
+}
+
+void rgb_toggle(int state)
+{
+    int cur_state = state % 3;
+    if(cur_state == 0)
     {
-        gd_rvstar_led_init(i);
-        gd_rvstar_led_on(i);
-        delay_1ms(1000);
-        gd_rvstar_led_off(i);
+        LED_G_ON;
+        LED_B_OFF;
+        LED_R_OFF;
+    }
+
+    if(cur_state == 1)
+    {
+        LED_G_OFF;
+        LED_B_ON;
+        LED_R_OFF;
+    }
+
+    if(cur_state == 2)
+    {
+        LED_G_OFF;
+        LED_B_OFF;
+        LED_R_ON;
     }
 }
+
 
 /*!
     \brief      main function
@@ -24,15 +60,13 @@ void led_config(void)
 */
 int main(void)
 {
-    led_config();
+    int state = 0;
+    user_gpio_init();
 
     while(1)
     {
-        for(int i=0; i<LEDn; i++)
-        {
-            gd_rvstar_led_toggle(i);
-            delay_1ms(1000);
-        }
+        state++;
+        delay_1ms(1000);
+        rgb_toggle(state);
     }
 }
-
