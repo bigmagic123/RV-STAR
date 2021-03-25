1c878a0ad4547d09e2e6bd28edbfdd1975cf2505
bigmagic <fujin.zhao@foxmail.com>
2021-03-25 13:16:24 +0800

    add key poll

--- projects/3_key_poll/application/main.c
+++ projects/3_key_poll/application/main.c
@@ -1,101 +1,112 @@
-// See LICENSE for license details.
+/*!
+    \file  main.c
+    \brief keyboard polling mode
+    
+    \version 2019-6-5, V1.0.0, firmware for GD32VF103
+*/
+
+/*
+    Copyright (c) 2019, GigaDevice Semiconductor Inc.
+
+    Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this 
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice, 
+       this list of conditions and the following disclaimer in the documentation 
+       and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holder nor the names of its contributors 
+       may be used to endorse or promote products derived from this software without 
+       specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+OF SUCH DAMAGE.
+*/
+
+
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include "nuclei_sdk_soc.h"
+#include "gd32vf103_libopt.h"
+#include "nuclei_sdk_hal.h"
 
-void print_misa(void)
+#define LED_G   LED1
+#define LED_B   LED2
+#define LED_R   LED3
+
+#define LED_G_ON     gd_rvstar_led_on(LED_G)
+#define LED_G_OFF    gd_rvstar_led_off(LED_G)
+
+#define LED_B_ON     gd_rvstar_led_on(LED_B)
+#define LED_B_OFF    gd_rvstar_led_off(LED_B)
+
+#define LED_R_ON     gd_rvstar_led_on(LED_R)
+#define LED_R_OFF    gd_rvstar_led_off(LED_R)
+
+//gpio init
+void user_gpio_init(void)
 {
-    CSR_MISA_Type misa_bits = (CSR_MISA_Type) __RV_CSR_READ(CSR_MISA);
-    static char misa_chars[30];
-    uint8_t index = 0;
-    if (misa_bits.b.mxl == 1) {
-        misa_chars[index++] = '3';
-        misa_chars[index++] = '2';
-    } else if (misa_bits.b.mxl == 2) {
-        misa_chars[index++] = '6';
-        misa_chars[index++] = '4';
-    } else if (misa_bits.b.mxl == 3) {
-        misa_chars[index++] = '1';
-        misa_chars[index++] = '2';
-        misa_chars[index++] = '8';
-    }
-    if (misa_bits.b.i) {
-        misa_chars[index++] = 'I';
-    }
-    if (misa_bits.b.m) {
-        misa_chars[index++] = 'M';
-    }
-    if (misa_bits.b.a) {
-        misa_chars[index++] = 'A';
-    }
-    if (misa_bits.b.b) {
-        misa_chars[index++] = 'B';
-    }
-    if (misa_bits.b.c) {
-        misa_chars[index++] = 'C';
-    }
-    if (misa_bits.b.e) {
-        misa_chars[index++] = 'E';
-    }
-    if (misa_bits.b.f) {
-        misa_chars[index++] = 'F';
-    }
-    if (misa_bits.b.d) {
-        misa_chars[index++] = 'D';
-    }
-    if (misa_bits.b.q) {
-        misa_chars[index++] = 'Q';
-    }
-    if (misa_bits.b.h) {
-        misa_chars[index++] = 'H';
-    }
-    if (misa_bits.b.j) {
-        misa_chars[index++] = 'J';
-    }
-    if (misa_bits.b.l) {
-        misa_chars[index++] = 'L';
-    }
-    if (misa_bits.b.n) {
-        misa_chars[index++] = 'N';
-    }
-    if (misa_bits.b.s) {
-        misa_chars[index++] = 'S';
-    }
-    if (misa_bits.b.p) {
-        misa_chars[index++] = 'P';
-    }
-    if (misa_bits.b.t) {
-        misa_chars[index++] = 'T';
-    }
-    if (misa_bits.b.u) {
-        misa_chars[index++] = 'U';
-    }
-    if (misa_bits.b.v) {
-        misa_chars[index++] = 'V';
-    }
-    if (misa_bits.b.x) {
-        misa_chars[index++] = 'X';
+    gd_rvstar_led_init(LED_G);
+    gd_rvstar_led_init(LED_B);
+    gd_rvstar_led_init(LED_R);
+
+    gd_rvstar_key_init(0, KEY_MODE_EXTI);
+}
+
+void rgb_toggle(int state)
+{
+    int cur_state = state % 3;
+    if(cur_state == 0)
+    {
+        LED_G_ON;
+        LED_B_OFF;
+        LED_R_OFF;
     }
 
-    misa_chars[index++] = '\0';
+    if(cur_state == 1)
+    {
+        LED_G_OFF;
+        LED_B_ON;
+        LED_R_OFF;
+    }
 
-    printf("MISA: RV%s\r\n", misa_chars);
+    if(cur_state == 2)
+    {
+        LED_G_OFF;
+        LED_B_OFF;
+        LED_R_ON;
+    }
 }
 
 int main(void)
 {
-    srand(__get_rv_cycle()  | __get_rv_instret() | __RV_CSR_READ(CSR_MCYCLE));
-    uint32_t rval = rand();
-    rv_csr_t misa = __RV_CSR_READ(CSR_MISA);
+    int state = 0;
+    user_gpio_init();
 
-    printf("MISA: 0x%lx\r\n", misa);
-    print_misa();
-
-    for (int i = 0; i < 20; i ++) {
-        printf("%d: Hello World From Nuclei RISC-V Processor!\r\n", i);
-    }
-
-    return 0;
+    while(1)
+    {
+        /* check whether the button is pressed */
+        if(SET ==  gd_rvstar_key_state_get(0))
+        {
+            delay_1ms(50);
+            /* check whether the button is pressed */
+            if(SET == gd_rvstar_key_state_get(0))
+            {
+                /* toggle the LED */
+                state++;
+                rgb_toggle(state);
+            }
+            
+            while(SET == gd_rvstar_key_state_get(0))
+            {
+            }
+        }
+    }    
 }
-
